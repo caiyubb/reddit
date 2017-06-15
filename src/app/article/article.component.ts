99842f4828cf18983178b7aa074da21a6c17b5bf
Caiyu <Caiyu@AppledeMacBook-Air-2.local>
2017-06-15 15:30:13 +0800

    [Cherri] feature : add article list

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -10,15 +10,8 @@ import { Article } from './article.model';
   }
 })
 export class ArticleComponent implements OnInit {
-  article: Article;
+  @Input() article: Article;
 
-  constructor() {
-    this.article = new Article(
-      'Angular 2',
-      'http://angular.io',
-      10
-    )
-   }
   voteUp():boolean {
     this.article.votes += 1;
     return false;
